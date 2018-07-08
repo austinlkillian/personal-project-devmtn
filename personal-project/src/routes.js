bcf604d1604cb180bc7eba18a777126a09e704a3
Monica Beauchamp <monica.j.beauchamp@gmail.com>
2018-07-08 15:17:58 -0600

    Add basic Login component and associated routing

--- personal-project/src/routes.js
+++ personal-project/src/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import SignUp from "./components/SignUp/SignUp";
 import Home from './components/Home/Home';
+import Login from './components/Login/Login'
 
 
 export default (
     <Switch>
         <Route exact path="/" component={Home}/>
         <Route path="/sign_up" component={SignUp}/>
+        <Route path="/login" component={Login} />
     </Switch>
 )
